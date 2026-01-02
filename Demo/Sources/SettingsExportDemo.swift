e51af9808078adda74c951e0eba0187e64eb669d
Aether <64797587+Aeastr@users.noreply.github.com>
2026-01-02 17:20:55 +0000

    Revert "Refactor SettingsState to Codable and remove UserDefaults"
    This reverts commit 6b1a0eaf8dcdf27ee499ec5ead0ce60b9584cffd.

--- Demo/Sources/SettingsExportDemo.swift
+++ Demo/Sources/SettingsExportDemo.swift
@@ -126,17 +126,6 @@ struct SettingsExportDemo: View {
 
     // MARK: - Export
 
-    // Keys we actually want to export (matches SettingsState properties)
-    private static let exportableKeys: Set<String> = [
-        "airplaneModeEnabled", "bluetoothEnabled", "personalHotspotEnabled",
-        "vpnQuickEnabled", "appleIntelligenceEnabled", "autoBrightness",
-        "siriSuggestions", "autoStandby", "debugMode", "verboseLogging",
-        "showHiddenFeatures", "networkDebugging", "airDropEnabled", "pipEnabled",
-        "autoFillPasswords", "use24Hour", "autoCorrect", "vpnManagementEnabled",
-        "darkMode", "autoJoinWiFi", "testToggle", "testSlider", "testText",
-        "testPicker", "testStepper", "testCounter"
-    ]
-
     private func prepareExport() async {
         isExporting = true
         defer { isExporting = false }
@@ -146,8 +135,7 @@ struct SettingsExportDemo: View {
             let config = SettingsExportConfiguration(
                 sources: [source],
                 appIdentifier: Bundle.main.bundleIdentifier ?? "com.settingskit.demo",
-                appVersion: Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
-                keyFilter: { Self.exportableKeys.contains($0) }
+                appVersion: Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
             )
 
             let data = try await SettingsPortable.exportSettingsData(configuration: config)
--- Demo/Sources/SettingsState.swift
+++ Demo/Sources/SettingsState.swift
@@ -1,33 +1,85 @@
 import SwiftUI
 
 @Observable
-class SettingsState: Codable {
-    var airplaneModeEnabled = false
-    var bluetoothEnabled = true
-    var personalHotspotEnabled = false
-    var vpnQuickEnabled = false
-    var appleIntelligenceEnabled = true
-    var autoBrightness = true
-    var siriSuggestions = true
-    var autoStandby = true
-    var debugMode = false
-    var verboseLogging = false
-    var showHiddenFeatures = false
-    var networkDebugging = false
-    var airDropEnabled = true
-    var pipEnabled = true
-    var autoFillPasswords = true
-    var use24Hour = false
-    var autoCorrect = true
-    var vpnManagementEnabled = false
-    var darkMode = false
-    var autoJoinWiFi = true
+class SettingsState {
+    var airplaneModeEnabled = UserDefaults.standard.bool(forKey: "airplaneModeEnabled") {
+        didSet { UserDefaults.standard.set(airplaneModeEnabled, forKey: "airplaneModeEnabled") }
+    }
+    var bluetoothEnabled = UserDefaults.standard.object(forKey: "bluetoothEnabled") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(bluetoothEnabled, forKey: "bluetoothEnabled") }
+    }
+    var personalHotspotEnabled = UserDefaults.standard.bool(forKey: "personalHotspotEnabled") {
+        didSet { UserDefaults.standard.set(personalHotspotEnabled, forKey: "personalHotspotEnabled") }
+    }
+    var vpnQuickEnabled = UserDefaults.standard.bool(forKey: "vpnQuickEnabled") {
+        didSet { UserDefaults.standard.set(vpnQuickEnabled, forKey: "vpnQuickEnabled") }
+    }
+    var appleIntelligenceEnabled = UserDefaults.standard.object(forKey: "appleIntelligenceEnabled") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(appleIntelligenceEnabled, forKey: "appleIntelligenceEnabled") }
+    }
+    var autoBrightness = UserDefaults.standard.object(forKey: "autoBrightness") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(autoBrightness, forKey: "autoBrightness") }
+    }
+    var siriSuggestions = UserDefaults.standard.object(forKey: "siriSuggestions") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(siriSuggestions, forKey: "siriSuggestions") }
+    }
+    var autoStandby = UserDefaults.standard.object(forKey: "autoStandby") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(autoStandby, forKey: "autoStandby") }
+    }
+    var debugMode = UserDefaults.standard.bool(forKey: "debugMode") {
+        didSet { UserDefaults.standard.set(debugMode, forKey: "debugMode") }
+    }
+    var verboseLogging = UserDefaults.standard.bool(forKey: "verboseLogging") {
+        didSet { UserDefaults.standard.set(verboseLogging, forKey: "verboseLogging") }
+    }
+    var showHiddenFeatures = UserDefaults.standard.bool(forKey: "showHiddenFeatures") {
+        didSet { UserDefaults.standard.set(showHiddenFeatures, forKey: "showHiddenFeatures") }
+    }
+    var networkDebugging = UserDefaults.standard.bool(forKey: "networkDebugging") {
+        didSet { UserDefaults.standard.set(networkDebugging, forKey: "networkDebugging") }
+    }
+    var airDropEnabled = UserDefaults.standard.object(forKey: "airDropEnabled") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(airDropEnabled, forKey: "airDropEnabled") }
+    }
+    var pipEnabled = UserDefaults.standard.object(forKey: "pipEnabled") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(pipEnabled, forKey: "pipEnabled") }
+    }
+    var autoFillPasswords = UserDefaults.standard.object(forKey: "autoFillPasswords") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(autoFillPasswords, forKey: "autoFillPasswords") }
+    }
+    var use24Hour = UserDefaults.standard.bool(forKey: "use24Hour") {
+        didSet { UserDefaults.standard.set(use24Hour, forKey: "use24Hour") }
+    }
+    var autoCorrect = UserDefaults.standard.object(forKey: "autoCorrect") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(autoCorrect, forKey: "autoCorrect") }
+    }
+    var vpnManagementEnabled = UserDefaults.standard.bool(forKey: "vpnManagementEnabled") {
+        didSet { UserDefaults.standard.set(vpnManagementEnabled, forKey: "vpnManagementEnabled") }
+    }
+    var darkMode = UserDefaults.standard.bool(forKey: "darkMode") {
+        didSet { UserDefaults.standard.set(darkMode, forKey: "darkMode") }
+    }
+    var autoJoinWiFi = UserDefaults.standard.object(forKey: "autoJoinWiFi") as? Bool ?? true {
+        didSet { UserDefaults.standard.set(autoJoinWiFi, forKey: "autoJoinWiFi") }
+    }
 
     // Test input state
-    var testToggle = false
-    var testSlider = 0.5
-    var testText = ""
-    var testPicker = 0
-    var testStepper = 0
-    var testCounter = 0
+    var testToggle = UserDefaults.standard.bool(forKey: "testToggle") {
+        didSet { UserDefaults.standard.set(testToggle, forKey: "testToggle") }
+    }
+    var testSlider = UserDefaults.standard.object(forKey: "testSlider") as? Double ?? 0.5 {
+        didSet { UserDefaults.standard.set(testSlider, forKey: "testSlider") }
+    }
+    var testText = UserDefaults.standard.string(forKey: "testText") ?? "" {
+        didSet { UserDefaults.standard.set(testText, forKey: "testText") }
+    }
+    var testPicker = UserDefaults.standard.integer(forKey: "testPicker") {
+        didSet { UserDefaults.standard.set(testPicker, forKey: "testPicker") }
+    }
+    var testStepper = UserDefaults.standard.integer(forKey: "testStepper") {
+        didSet { UserDefaults.standard.set(testStepper, forKey: "testStepper") }
+    }
+    var testCounter = UserDefaults.standard.integer(forKey: "testCounter") {
+        didSet { UserDefaults.standard.set(testCounter, forKey: "testCounter") }
+    }
 }
